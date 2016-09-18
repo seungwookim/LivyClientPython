5c6e53a10875caa17071f73e11a6d67c811da588
seungwookim <tmddno1@naver.com>
2016-09-18 03:07:43 +0000

    json format get/set , append, desc table , bug fixs

--- livy_df_client.py
+++ livy_df_client.py
@@ -1,19 +1,22 @@
-import json, requests, textwrap, time, random
+import json, requests, textwrap, time, random, os
 
-# json object hooker class
+
 class JsonObject:
-  def __init__(self, d):
-    self.__dict__ = d
+    """
+    json object hooker class
+    """
+    def __init__(self, d):
+        self.__dict__ = d
 
-#json_data = json.loads(data, object_hook=JsonObject)
-#requests.get('http://httpbin.org', hooks=dict(response=print_url))
-#list(filter(lambda x:x=='idle' ,self.alive_sess_obj))
+    def __getitem__(self, item):
+        return self.__dict__[item]
+
 
 class LivyParqClientManager:
     def __init__(self, s_num):
         self.max_sess_num = s_num
-        self.host = "http://481bf68ee6d9:8998"
-        self.hdfs_path = "/home/dev/hadoop/data_frame"
+        self.host = ""
+        self.hdfs_path = ""
         self.headers = {'Content-Type': 'application/json'}
         self.alive_sess_obj = None
         self.alive_sess_cnt = None
@@ -21,9 +24,27 @@ class LivyParqClientManager:
         self.alive_sess_state = []
         self.avail_sess_list = []
 
+        self.load_conf()
+
+    def load_conf(self):
+        """
+        load livy config from file
+        :return: None
+        """
+        try :
+            workdir = os.path.dirname(os.getcwd())
+            livy_conf = open(workdir + "/LivyClientPython/tfmsa_settings.json" , 'r')
+            json_data = json.loads(livy_conf.read(), object_hook=JsonObject)
+            self.host = "http://" + json_data.livy.ip + ":" + json_data.livy.port
+            self.hdfs_path = json_data.hdfs.path
+            livy_conf.close()
+        except IOError as e:
+            return e
+
+
     def create_session(self):
         """
-        create session, get session id form return, run long code with that session
+        create session is required before any actions
         :return:
         """
         self.check_alive_sessions()
@@ -38,7 +59,8 @@ class LivyParqClientManager:
                 "driverCores": 1,
                 "driverMemory": "512m"}
         r = requests.post(self.host + "/sessions", data=json.dumps(data), headers=self.headers)
-        return r.json()['id']
+        result = livy_client.get_response(str(r.json()['id']), None)
+        return result
 
     def check_alive_sessions(self):
         """
@@ -71,14 +93,13 @@ class LivyParqClientManager:
             for i in range(0 , self.alive_sess_cnt):
                 if(self.alive_sess_obj.sessions[i].state == 'idle'):
                     self.avail_sess_list.append(self.alive_sess_obj.sessions[i].id)
-        print("list of available sessions : {0} " .format(self.avail_sess_list))
 
     def delete_all_sessions(self):
         """
         delete all sessions
-        :return:
+        :return: None
         """
-        print(self.alive_sess_list)
+        self.check_alive_sessions()
         for sess_id in self.alive_sess_list:
             print(sess_id)
             r = requests.delete(self.host + "/sessions/" + str(sess_id), headers=self.headers)
@@ -86,8 +107,8 @@ class LivyParqClientManager:
 
     def print_all(self):
         """
-        delete all sessions
-        :return:
+        print all conifg vales of Class
+        :return: None
         """
         print("host : {0}".format(self.host))
         print("headers : {0}".format(self.headers))
@@ -98,100 +119,153 @@ class LivyParqClientManager:
 
     def create_table(self, table_name, json_data):
         """
-        action for create table with json request
-        :return:
+        create table with json data
+        :param table_name: name of table want to create
+        :param json_data: json form schema data
+        :return: success or failure
         """
         self.get_available_sess_id()
         data = {
             'code': ''.join(['from pyspark.sql import SQLContext, DataFrameWriter, DataFrame\n',
                              'sqlContext = SQLContext(sc)\n',
                              'df_writer = sqlContext.createDataFrame(', str(json_data)  ,').write\n',
-                             'df_writer.parquet("' , str(self.hdfs_path), "/", table_name , '", mode="overwrite", partitionBy=None)'
-                             # 'df_writer.save(path="' , str(self.hdfs_path),'/', table_name ,'", source= "' , table_name , '" ,mode="overwrite" )'
-                             #'df_writer.saveAsTable("' , table_name , '",format="parquet", mode="overwrite" , partitionBy=None)'
-                             # 'df_writer = DataFrameWriter(df)\n',
-                             # 'df_writer'
-                             #'df.write.format("parquet").save("' ,str(self.hdfs_path), "/", table_name , '.parquet")\n'
-                             # 'result = df_writer.saveAsTable("' ,
-                             # table_name ,'", format="parquet", mode="overwrite"',
-                             # ', path="' ,str(self.hdfs_path), "/", table_name, ' " ' ,
-                             # ')\n',
-                             # 'result'
+                             'df_writer.parquet("' , str(self.hdfs_path), "/", table_name ,
+                             '", mode="overwrite", partitionBy=None)'
                              ])
         }
 
         print("request codes : {0} ".format(data))
-        resp = requests.post(self.host + "/sessions/" + str(min(self.avail_sess_list)) + "/statements", data=json.dumps(data), headers=self.headers)
+        resp = requests.post(self.host + "/sessions/" + str(min(self.avail_sess_list)) + \
+                             "/statements", data=json.dumps(data), headers=self.headers)
         temp_resp = json.loads(resp.content, object_hook=JsonObject)
         result = livy_client.get_response(str(min(self.avail_sess_list)), temp_resp.id)
         print("result : {0} ".format(result))
 
+    def append_data(self, table_name, json_data):
+        """
+        append data on exist table
+        :param table_name: name of table want to add data
+        :param json_data: json form schema data
+        :return: success or failure
+        """
+        self.get_available_sess_id()
+        data = {
+            'code': ''.join(['from pyspark.sql import SQLContext, DataFrameWriter, DataFrame\n',
+                             'sqlContext = SQLContext(sc)\n',
+                             'df_writer = sqlContext.createDataFrame(', str(json_data)  ,').write\n',
+                             'df_writer.parquet("' , str(self.hdfs_path), "/", table_name ,
+                             '", mode="append", partitionBy=None)'
+                             ])
+        }
+
+        print("request codes : {0} ".format(data))
+        resp = requests.post(self.host + "/sessions/" + str(min(self.avail_sess_list)) + \
+                             "/statements", data=json.dumps(data), headers=self.headers)
+        temp_resp = json.loads(resp.content, object_hook=JsonObject)
+        result = livy_client.get_response(str(min(self.avail_sess_list)), temp_resp.id)
+        print("result : {0} ".format(result))
 
     def get_response(self, session_id, statements_id):
         """
-        retry till running finished
-        :return:
+        wait for Livy running and get return when finished
+        :param session_id:session id used on request
+        :param statements_id: statements id used on request
+        :return: response result from livy
         """
-        resp = requests.get(self.host + "/sessions/" + str(session_id) + "/statements/" + str(statements_id), headers=self.headers)
+        if(statements_id == None):
+            resp = requests.get(self.host + "/sessions/" + str(session_id)  \
+                                , headers=self.headers)
+        else:
+            resp = requests.get(self.host + "/sessions/" + str(session_id) +  \
+                                "/statements/" + str(statements_id), \
+                                headers=self.headers)
+
         response_obj = json.loads(resp.content, object_hook=JsonObject)
 
         if(response_obj.state == 'running'):
             time.sleep(1)
             return self.get_response(session_id, statements_id)
+        elif(response_obj.state == 'starting'):
+            time.sleep(1)
+            return self.get_response(session_id, statements_id)
         else:
+            print("Response : {0}".format(resp.json()))
             return resp.json()
 
 
 
     def query_data(self, table_name, query_str):
         """
-        get data from hive table
-        :return:
+        get query data from spark
+        :param table_name: name of table you want to get data
+        :param query_str: sql strings
+        :return: query result as json Object
         """
 
         self.get_available_sess_id()
 
         data = {
             'code': ''.join(['from pyspark.sql import SQLContext\n',
+                             'import json\n',
                              'sqlContext = SQLContext(sc)\n',
-                             'rows = sqlContext.read.load("' , str(self.hdfs_path), "/", table_name , '" , "parquet" )\n',
-                             'tbl = rows.registerTempTable("' , table_name , '")\n'
-                             'result = sqlContext.sql("' , str(query_str) ,'").collect()\n'
+                             'rows = sqlContext.read.load("' , str(self.hdfs_path),
+                             "/" ,table_name , '" , "parquet" )\n',
+                             'tbl = rows.registerTempTable("' , table_name , '")\n',
+                             'result = sqlContext.sql("' , str(query_str) ,
+                             '").toJSON(False).map(lambda x : x).collect()\n',
                              'result'
                              ])
         }
 
-        resp = requests.post(self.host + "/sessions/" + str(min(self.avail_sess_list)) + "/statements", data=json.dumps(data), headers=self.headers)
-        temp_resp = json.loads(resp.content, object_hook=JsonObject)
-        result = livy_client.get_response(str(min(self.avail_sess_list)), temp_resp.id)
-        print("result : {0} ".format(result))
+        resp = requests.post(self.host + "/sessions/" + str(min(self.avail_sess_list)) + \
+                             "/statements", data=json.dumps(data), headers=self.headers)
+        result = livy_client.get_response(str(min(self.avail_sess_list)), \
+                                          json.loads(resp.content, object_hook=JsonObject).id)
+        result = json.loads(result["output"]["data"]["text/plain"].replace("'", ""), \
+                            object_hook=JsonObject)
+        return result
 
 
-    def query_stucture(self, table_name, query_str):
+    def query_stucture(self, table_name):
         """
-        get data from hive table
+        get table structure info
         :return:
         """
-
         self.get_available_sess_id()
 
         data = {
             'code': ''.join(['from pyspark.sql import SQLContext\n',
                              'sqlContext = SQLContext(sc)\n',
-                             'rows = sqlContext.read.load("', str(self.hdfs_path), "/", table_name, '" , "parquet" )\n',
-                             'tbl = rows.registerTempTable("', table_name, '")\n'
-                             'result = sqlContext.sql("', str(query_str),'")\n'
-                             'result'
+                             'rows = sqlContext.read.load("', str(self.hdfs_path),
+                              "/" , table_name, '" , "parquet" ).schema.json()\n',
+                             'rows'
                              ])
         }
 
-        resp = requests.post(self.host + "/sessions/" + str(min(self.avail_sess_list)) + "/statements",
-                             data=json.dumps(data), headers=self.headers)
-        temp_resp = json.loads(resp.content, object_hook=JsonObject)
-        result = livy_client.get_response(str(min(self.avail_sess_list)), temp_resp.id)
-        print("result : {0} ".format(result))
+        resp = requests.post(self.host + "/sessions/" + str(min(self.avail_sess_list)) + \
+                             "/statements", data=json.dumps(data), headers=self.headers)
+        result = livy_client.get_response(str(min(self.avail_sess_list)), \
+                                          json.loads(resp.content, object_hook=JsonObject).id)
+        result = json.loads(result["output"]["data"]["text/plain"].replace("'", ""), \
+                            object_hook=JsonObject)
+        return result
 
+
+# sample codes
 livy_client = LivyParqClientManager(2)
-#livy_client.create_session()
-livy_client.create_table("xxxx", "[{'name':'Andy', 'univ':'snu'},{'name':'Kim', 'univ':'snu'} ]")
-livy_client.query_data("xxxx", "select * from xxxx")
+livy_client.create_session()
+#livy_client.delete_all_sessions()
+livy_client.create_table("xxxx", "[{'name':'Andy', 'univ':'snu'},{'name':'Kim', 'univ':'snu'}," \
+                                 "{'name':'a', 'univ':'snu'},{'name':'b', 'univ':'snu'} ," \
+                                 "{'name':'c', 'univ':'snu'},{'name':'d', 'univ':'snu'}   ]")
+livy_client.append_data("xxxx", "[{'name':'ADDDDD', 'univ':'ADDDDD'}]")
+out = livy_client.query_data("xxxx", "select * from xxxx")
+forms = livy_client.query_stucture("xxxx")
+
+for j in range(0, len(forms.fields)):
+    print(forms.fields[j].name)
+
+for i in range(0, len(out)):
+    for j in range(0, len(forms.fields)):
+        print("=== {0}".format(out[i][forms.fields[j].name]))
+
